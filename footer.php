23b59f141f902c3be10702e3658bc13d6aa86e7a
vadyvarela <vadyy2014@gmail.com>
2019-02-28 16:54:03 -0100

    Pagina Vender propriedade Criada

---
+++ footer.php
@@ -0,0 +1,21 @@
+
+  <!-- footer -->
+  <div class="footer">
+    <div class="container">
+      <p>2018 © ImoAfrica. Todos os direitos reservados<br>
+        Implementado pela <a href="#">iMedia Innovative media</a>
+        <a class="facebooklink" href="#"> FACEBOOK </a>
+      </p>
+      
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="js/script.js"></script>
+  <script src="js/jssorslider.js"></script>
+
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Imoafrica</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/index.css" rel="stylesheet">
+  <link href="css/jssorlider.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+    <div class="container">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="">
+          <a class="navbar-brand" href="#">
+            <img src="images/logop.png" alt="" class="logo">
+          </a>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Arrendar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Comprar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Vender</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Legalizar</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contactos</a>
+          </li>
+        </ul>
+
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Junte-se a nós</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Avaliação</a>
+          </li>
+          </li>
+        </ul>
+
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link bold" href="#"> <img src="images/phone.png" width="30" height="30" alt="" class=""> (+238) 921 01 15</a>
+          </li>
+          <li>
+          <div class="btn-group">
+            <a class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <img width="40" height="40" src="images/pt.png">
+            </a>
+            
+            <ul class="dropdown-menu">
+              <li>
+                <a href="#" title="Select this card"><img width="40" height="40" src="images/fr.png"></a>
+              </li>
+              <li>
+                <a href="#" title="Select this card"><img width="40" height="40" src="images/en.png"></a>
+              </li>
+            </ul>
+          </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
