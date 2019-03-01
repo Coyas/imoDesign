32fdce66b700833a8c60aa04e6e558fb3ac6f7b2
vadyvarela <vadyy2014@gmail.com>
2019-03-01 18:14:17 -0100

    Pagina Cadastra propridade e contato criada

---
+++ contato.php
@@ -0,0 +1,56 @@
+<div class="">
+<div class="meucontainer">
+    <div class="row">
+        <div style="background-color: #fff;" class="col-md-6">
+            MAPA
+        </div>
+        <div class="contatodesc col-md-6">
+            <div class="contato">
+            <h1 class="titlecontato"> Contatos </h1>
+                <div class="dadosendereco">
+                    <span class="itemscontato"> <img src="images/tele.png" width="20" height="20" alt=""> (+238) 298 76 77 </span>
+                    <span class="itemscontato"> <img src="images/email.png" width="20" height="20" alt=""> geral@imoafrica.com </span>
+                    <span class="itemscontato"> <img src="images/endereco.png" width="20" height="20" alt=""> Rua Caixa Economica, ao lado INE Fazenda, Praia Santiago cv, Praia 7300 </span>
+                    <span class="itemscontato"> <img src="images/aberto.png" width="20" height="20" alt=""> Aberto de Segunda á Sexta das 8:00Hr ás 19:00Hr </span>
+                
+                </div>
+            </div>
+
+            <div class="formfale">
+                <h1 class="title-form"> Fale Connosco </h1>
+                <form action="">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="input-group col-md-12">
+                                <input class="inputcontato" type="text" placeholder="Nome">
+                            </div>
+                            <div class="input-group col-md-12">
+                                <input class="inputcontato" type="email" placeholder="E-mail">
+                            </div>
+                            
+                            <div class="input-group col-md-12">
+                                <select class="inputcontato" name="" id="">
+                                    <optgroup>
+                                        <option value=""> Assunto </option>
+                                        <option value="">1</option>
+                                    </optgroup>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6"> 
+                        <div class="input-group col-md-12">
+                            <textarea class="col-md-12 meutextareac" name="" id="" rows="4" placeholder="Mensagem"></textarea>
+                        </div> 
+                    </div>
+                </div>
+                </form>
+            </div>
+            
+        </div>
+    </div>
+</div>
+</div>
+
--- index.php
+++ index.php
@@ -9,11 +9,14 @@
   <meta name="author" content="">
 
   <title>Imoafrica - Real Estate</title>
-
   <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="css/index.css" rel="stylesheet">
   <link href="css/jssorlider.css" rel="stylesheet">
+  <link href="css/meucss.scss" rel="stylesheet">
+  <link rel='stylesheet' href='https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'>
+
+<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.2/css/bootstrapValidator.min.css"/>
 
 </head>
 
@@ -24,7 +27,11 @@
 ?>
 
 <?php 
-  require('body.php');
+  //require('body.php');
+  //require('vender.php');
+  //require('comprar.php');
+  //require('cadpropriedade.php');
+  require('contato.php');
 ?>
 
 <?php 
@@ -34,6 +41,9 @@
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src='https://static.codepen.io/assets/common/stopExecutionOnTimeout-de7e2ef6bfefd24b79a3f68b414b87b8db5b08439cac3f1012092b2290c719cd.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>
+  <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.2/js/bootstrapValidator.min.js"></script>
   <script src="js/script.js"></script>
   <script src="js/jssorslider.js"></script>
 
